5790e2ba61e842869ddddcb6689f57109a14552f
Ashish.s.panicker <Ashish.s.panicker@gmail.com>
2023-11-21 09:56:49 +0530

    init commit

---
+++ src/main/java/com/example/springmvcdemo/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.example.springmvcdemo;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message = "Welcome to Java Web Applications";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+}
